f0b533e6b45f2d10a751b20873366a82cd1e9f65
Wize669 <william.zapata@epn.edu.ec>
2024-08-24 19:38:40 -0500

    feat: Develop the menu screen of the administration module

--- interfaces/AuthContext.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "@/interfaces/User";
-import React from "react";
-
-export interface AuthContextInterface {
-    authUser: User | null;
-    setAuthUser: React.Dispatch<React.SetStateAction<User | null>>;
-    isLoggedIn: boolean;
-    setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
-}
